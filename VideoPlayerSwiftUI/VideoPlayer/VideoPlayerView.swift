fc9ad770deabf747b3f0377dc4523ad4fd669be2
Salvatore Palazzo <salvatop78@gmail.com>
2024-01-16 12:08:22 -0500

    add progress view to handle ViewModel loading

--- VideoPlayerSwiftUI/VideoPlayer/VideoPlayerView.swift
+++ VideoPlayerSwiftUI/VideoPlayer/VideoPlayerView.swift
@@ -1,35 +1,32 @@
 //
-//  ContentView.swift
+//  VideoPlayerView.swift
 //  VideoPlayerSwiftUI
 //
 //  Created by Michael Gauthier on 2021-01-06.
 //
 
 import SwiftUI
-import AVKit
 
 struct VideoPlayerView: View {
     @EnvironmentObject var viewModel: VideoPlayerViewModel
-    
+
+
     var body: some View {
         VStack {
             Text("Video Player")
                 .font(.title)
-            ZStack {
-                CustomVideoPlayerView(videoUrls: viewModel.videoUrlList)
+            if viewModel.isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+            } else {
+                ZStack {
+                    CustomVideoPlayerView(videoUrls: viewModel.videoUrlList)
                     .frame(height: 220)
-            }
-            .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack{
-                    ScrollView {
-                        VideoDetailsView(videoList: viewModel.videoUrlList)
-                        Spacer()
-                    }
-                    .background(Color.white)
                 }
-                .padding()
-                Spacer()
+                .edgesIgnoringSafeArea(.all)
+                VStack {
+                    VideoDetailsView(videoList: viewModel.videoUrlList)
+                }
             }
         }
         .preferredColorScheme(.dark)
