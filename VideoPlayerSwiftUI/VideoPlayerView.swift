c470ab0ca9b03820a6461816112f454dca123512
Salvatore Palazzo <salvatop78@gmail.com>
2024-01-16 10:53:30 -0500

    add layout to contains the video on top and description at the bottom of it

--- VideoPlayerSwiftUI/VideoPlayerView.swift
+++ VideoPlayerSwiftUI/VideoPlayerView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct VideoPlayerView: View {
+    let videoUrls = [URL(string: "https://d140vvwqovffrf.cloudfront.net/media/5e87b9a811599/hls/index.m3u8")]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Video Player")
+                .font(.title)
+            ZStack {
+                VideoPlayer(player: AVPlayer())
+                    .frame(height: 220)
+            }
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack{
+                    Text("Description")
+                    Spacer()
+                }
+                .padding()
+                Spacer()
+            }
+        }
+        .preferredColorScheme(.dark)
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        VideoPlayerView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            VideoPlayerView()
+        }
     }
 }
