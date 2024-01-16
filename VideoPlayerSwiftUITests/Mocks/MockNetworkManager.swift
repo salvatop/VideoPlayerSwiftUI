66747f92dbdeebd34faac80517660ac94dcc624b
Salvatore Palazzo <salvatop78@gmail.com>
2024-01-16 11:48:12 -0500

    add logic to mock network calls for unit test

---
+++ VideoPlayerSwiftUITests/Mocks/MockNetworkManager.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class MockNetworkManager: NetworkManagerProtocol {
+    var sendRequestResult: Result<[Video], NetworkManager.ApiError>?
+
+    func sendHTTPRequest(urlString: String, dataModel: Decodable.Type) async throws -> Result<[Video], NetworkManager.ApiError> {
+        guard let result = sendRequestResult else {
+            throw NetworkManager.ApiError.unknownError
+        }
+        return result
+    }
+}
